4112b41777ca8408a8ce884c5cbe1d81aeedd093
tsudath <obsbowl.tsuda@gmail.com>
2023-06-13 21:25:57 +0900

    import winpty

--- mnist.py
+++ mnist.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing import image
 from keras.optimizers import adam
 import numpy as np
-
+import winpty
 
 classes = ["0","1","2","3","4","5","6","7","8","9"]
 image_size = 28
