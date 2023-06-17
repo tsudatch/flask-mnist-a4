a541382db7c9d799c688cf06f7756017ef0bb751
tsudatch <obsbowl.tsuda@gmail.com>
2023-06-17 14:21:04 +0900

    ライブラリをインポート

--- mnist.py
+++ mnist.py
@@ -1,4 +1,4 @@
-
+# cording: utf-8
 import os
 from keras.datasets import mnist
 from keras.layers import Dense, Dropout, Flatten, Activation
@@ -9,10 +9,18 @@ from keras.utils.vis_utils import plot_model
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import optimizers
+from tensorflow.keras import adam
 from tensorflow import keras
 from keras.optimizers import adam
 import cv2
-from flask import Flask
+from flask import Flask, request, redirect, render_template, flash
+
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+
+
+
 
 #from winpty import PtyProcess
 
@@ -28,68 +36,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 model = load_model('./model.h5', compile=False)#学習済みモデルをロード
-
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('ファイルがありません')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('ファイルがありません')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            filepath = os.path.join(UPLOAD_FOLDER, filename)
-
-            #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
-            img = image.img_to_array(img)
-            data = np.array([img])
-            #変換したデータをモデルに渡して予測する
-            result = model.predict(data)[0]
-            predicted = result.argmax()
-            pred_answer = "これは " + classes[predicted] + " です"
-
-            return render_template("index.html",answer=pred_answer)
-
-    return render_template("index.html",answer="")
-
-
-if __name__ == "__main__":
-   port = int(os.environ.get('PORT', 8080))
-   app.run(host ='0.0.0.0',port = port)
-=======
-import os
-from keras.datasets import mnist
-from keras.layers import Dense, Dropout, Flatten, Activation
-from keras.layers import Conv2D, MaxPooling2D
-from keras.models import Sequential, load_model
-from keras.utils.np_utils import to_categorical
-from keras.utils.vis_utils import plot_model
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras import optimizers
-from tensorflow import keras
-from keras.optimizers import adam
-import cv2
-from winpty import PtyProcess
-
-classes = ["0","1","2","3","4","5","6","7","8","9"]
-image_size = 28
-
-UPLOAD_FOLDER = "uploads"
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-app = Flask(__name__)
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-model = load_model('./model.h5')#学習済みモデルをロード
 
 
 @app.route('/', methods=['GET', 'POST'])
